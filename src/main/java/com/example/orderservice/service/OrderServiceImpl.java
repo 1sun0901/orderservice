ba4978e49b54b4f8bbf70eb53571bbdce663faaf
1sun0901 <emuce@naver.com>
2022-07-06 20:39:27 +0900

    주문 로직 적용

---
+++ src/main/java/com/example/orderservice/service/OrderServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.jpa.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService{
+
+    private final OrderRepository orderRepository;
+
+    @Override
+    public OrderDto createOrder(OrderDto orderDto) {
+
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDto.getQty() * orderDto.getUnitPrice());
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        OrderEntity orderEntity = mapper.map(orderDto, OrderEntity.class);
+//        orderEntity.setEncryptedPwd(passwordEncoder.encode(orderDto.getPwd()));
+
+        orderRepository.save(orderEntity);
+
+        OrderDto returnOrderDto = mapper.map(orderEntity, OrderDto.class);
+
+        return returnOrderDto;
+
+    }
+
+    @Override
+    public OrderDto getOrderByOrderId(String orderId) {
+        OrderEntity orderEntity = orderRepository.findByOrderId(orderId);
+        ModelMapper mapper = new ModelMapper();
+        OrderDto orderDto = mapper.map(orderEntity, OrderDto.class);
+        return orderDto;
+    }
+
+    @Override
+    public Iterable<OrderEntity> getOrderByUserId(String userId) {
+        return orderRepository.findByUserId(userId);
+    }
+}
